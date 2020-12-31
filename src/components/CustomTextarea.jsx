8c7b03466aa9013b1a529e8d69bc25c7083f27b6
MariaChakhtoura <maria.chakhtoura@aqeed.com>
2020-12-31 15:08:12 +0200

    MC - added test api call

--- src/components/CustomTextarea.jsx
+++ src/components/CustomTextarea.jsx
@@ -8,20 +8,41 @@ const CustomTextarea = () => {
     <CKEditor
       editor={ClassicEditor}
       data="<p>Hello from CKEditor 5!</p>"
-      onReady={(editor) => {
-        // You can store the "editor" and use when it is needed.
-        console.log('Editor is ready to use!', editor);
-      }}
+      onReady={(editor) => {}}
       onChange={(event, editor) => {
         const data = editor.getData();
-        console.log({ event, editor, data });
+        fetch(
+          `https://api.yamli.com/transliterate.ashx?word=${'s'}&tool=api&account_id=000006&prot=https%3A&hostname=www.yamli.com&path=%2Farabic-keyboard%2F&build=5515&sxhr_id=15`,
+          {
+            headers: {
+              'Accept-Language': 'en-US,en;q=0.5',
+              'Accept-Encoding': 'gzip, deflate, br',
+              'Access-Control-Allow-Origin': '*',
+            },
+            referrer: 'https://www.yamli.com/arabic-keyboard/',
+          }
+        )
+          .then((response) => response.json())
+          .then((response) => {
+            let items;
+            const matchedData = response
+              .toString()
+              .match(/(?<=\{"data":")(.*?)(?=",")/);
+            if (matchedData && matchedData.length > 0) {
+              // const itemObjects = JSON.parse( matchedData[ 0 ] );
+              // const rItems = JSON.parse( itemObjects.data ).r;
+              const rItems = matchedData[0];
+              const arabicChoices = rItems.match(/[\u0621-\u064A ]+/g);
+              const totalChoices = [queryText, ...arabicChoices];
+              // console.log( arabicChoices );
+              console.log(totalChoices);
+            } else {
+              console.log([]);
+            }
+          });
       }}
-      onBlur={(event, editor) => {
-        console.log('Blur.', editor);
-      }}
-      onFocus={(event, editor) => {
-        console.log('Focus.', editor);
-      }}
+      onBlur={(event, editor) => {}}
+      onFocus={(event, editor) => {}}
     />
   );
 };
